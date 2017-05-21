0c35d47d52f93a28eef30a9d7f3d4fb2409bf498
cc <1015381017@qq.com>
2017-05-21 21:51:35 +0800

    Third party plug-in

--- source/js/concise.js
+++ source/js/concise.js
@@ -1,18 +1,56 @@
 $(function(){
-    var $div=$getEClass('page-content');
-    for(var i=0;i<$div.length;i++){
-        console.log($div[i].innerText);
-        $div[i].innerText=$div[i].innerText.replace(/\r|\n/ig,'').substring(0,350)+'...';
+    var $archivePage=$getEClass('archive-page');
+    var num=0;
+    if($archivePage&&$archivePage.length!=0){
+        var time=setInterval(function(){
+            $archivePage[num].setAttribute('class',$archivePage[num].getAttribute('class')+' transition');
+            num++;
+            if(num==$archivePage.length){
+                num=0;
+                clearInterval(time);
+            }
+        },150);
+    }
+    var $a=document.getElementById('my-search-a');
+    document.onkeydown=function(ev){
+        var ev=ev||event;
+        if(ev.altKey&&ev.ctrlKey){
+            $a.click();
+        }
+    };
+});
+addevent(window,'load',function(){
+    var $imgs=document.getElementById('page-post-content').getElementsByTagName('img');
+    console.log($imgs);
+    var $aside=document.createElement('aside');
+    $aside.setAttribute('id','popup');
+    var htm=[
+        '<div class="popup-content">',
+            '<img src="">',
+            '<span class="popup-left"></span>',
+            '<span class="popup-right"></span>',
+        '</div>'
+    ].join('');
+    $aside.innerHTML=htm;
+    for(var i=0;i<$imgs.length;i++){
+        $imgs[i].onclick=function(){
+            document.body.appendChild($aside);
+            document.body.style.overflow='hidden';
+        }
     }
 });
+function addevent($ele,ev,fun,tab){
+    $ele.addEventListener ? $ele.addEventListener(ev,fun,tab||false)
+        : $ele.attachEvent('on'+ev,fun);
+}
 function $(fun){
     window.onload=fun;
 }
 function $getEId(id){
-    return document.getELementById(id);
+    return document.getElementById(id);
 }
 function $getETag(tag){
-    return document.getELementsByTagName(tag);
+    return document.getElementsByTagName(tag);
 }
 function $getEClass(clas){
     return document.getElementsByClassName(clas);
